02dac2d2171a1db892c650e03b04554e959af8c9
FosterSOAsare <fostersoasare@gmail.com>
2023-12-06 21:11:25 +0000

    Admin middleware

--- middlewares/admin.middleware.js
+++ middlewares/admin.middleware.js
@@ -1,5 +1,8 @@
 function admin(req, res, next) {
 	// Write logic here soon
+	if (req.user.role !== "admin") {
+		res.status(401).json({ error: "User not authorized to perform this action" });
+	}
 	next();
 }
 
