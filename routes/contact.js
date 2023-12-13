9cb406b417675b2344f3b444e13a2dde953365f8
FosterSOAsare <fostersoasare@gmail.com>
2023-12-13 21:42:49 +0000

    Contact refactored. Only authenticated users can send a contact

--- routes/contact.js
+++ routes/contact.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { sendContactMessage, notifyUserOfContact } = require("../libs/nodemailer");
 const contactsRouter = express.Router();
 
+const user = require("../middlewares/user.middleware");
+
 const postContactMessage = async (req, res) => {
-	const { name, email, message } = req.body;
-	if (!name || !email || !message) {
+	const { name, message } = req.body;
+	const { email } = req.user;
+	if (!name || !message) {
 		res.status(401).json({ error: "Please provide all required fields" });
 	}
 	// Send contact message
@@ -14,6 +17,6 @@ const postContactMessage = async (req, res) => {
 	res.status(200).json({ success: true, message: "Contact message sent successfully" });
 };
 
-contactsRouter.post("/", postContactMessage);
+contactsRouter.post("/", user, postContactMessage);
 
 module.exports = contactsRouter;
